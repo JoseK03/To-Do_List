10c1722ee2320da38e55533e879b267dd2dcb752
JoseK03 <dajoseva@gmail.com>
2024-01-10 11:15:46 -0500

    feat: :white_check_mark: instalacion de morgan
    para abstraccion de informacion sobre las peticiones del back realizadas por clientes

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,10 @@
 import express from "express";
 import dotenv from 'dotenv';
 dotenv.config();
+import morgan from "morgan";
 
 const app = express();
+
+app.use(morgan('combined'));
 
 export default app;
