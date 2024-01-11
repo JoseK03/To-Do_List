79c21fc3efebd52d7e7d9d75266a064ffc7b935b
JoseK03 <dajoseva@gmail.com>
2024-01-11 10:54:48 -0500

    feat: :sparkles: Inicio creacion rutas para login y register

--- Backend/app.js
+++ Backend/app.js
@@ -2,9 +2,11 @@ import express from "express";
 import dotenv from 'dotenv';
 dotenv.config();
 import morgan from "morgan";
+import authRoutes from "./src/routes/auth.routes.js";
 
 const app = express();
 
 app.use(morgan('combined'));
+app.use(authRoutes);
 
 export default app;
