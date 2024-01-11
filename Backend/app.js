066e46065b634d1560aaf7f1bd8ec3d5da34d20b
JoseK03 <dajoseva@gmail.com>
2024-01-11 12:13:02 -0500

    feat: :sparkles: llenado de prueba
    Llenado de prueba a la bd para corroborar funcionamiento para la ruta de usuarios

--- Backend/app.js
+++ Backend/app.js
@@ -7,6 +7,9 @@ import authRoutes from "./src/routes/auth.routes.js";
 const app = express();
 
 app.use(morgan('combined'));
-app.use(authRoutes);
+app.use(express.json());
+
+ 
+app.use('/api', authRoutes);
 
 export default app;
--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,3 +1,23 @@
-export const register = (req, res) => res.send('register');
+import User from '../models/User.model.js'
+
+export const register = async (req, res) => {
+     const {email, password, username} = req.body;
+     
+     try {
+        const newUser = new User({
+            username,
+            password,
+            email
+         })
+         
+         await newUser.save();
+         res.send('registrando...')
+         console.log(newUser);
+     } catch (error) {
+        console.log(error);
+     }
+
+     
+};
 
 export const login = (req,res) => res.send('login');
