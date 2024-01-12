f25e2d9e8d24553a66a7b565df0383cfd1e2dc66
JoseK03 <dajoseva@gmail.com>
2024-01-12 11:34:29 -0500

    feat: :sparkles: realizacion de login
    Se realiza las funciones de register, login y logout

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,8 @@
+//Instalamos las dependencias necesarias para levantar el servidor
 import express from "express";
 import dotenv from 'dotenv';
 dotenv.config();
+//Morgan nos permite obtener informacion del usuario que realice una peticion
 import morgan from "morgan";
 import authRoutes from "./routes/auth.routes.js";
 
--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,22 +1,58 @@
 import User from '../models/User.model.js'
+import bcrypt from 'bcryptjs' // usado para encriptar la password
+import createAccessToken from '../libs/jwt.js';
 
 export const register = async (req, res) => {
      const {email, password, username} = req.body;
      try {
-        const newUser = new User({
+
+         const passwordHash = await bcrypt.hash(password, 10)
+
+         const newUser = new User({
             username,
-            password,
-            email
-         })
+            email,
+            password: passwordHash
+         });
+         
+         const userSaved = await newUser.save();
+         const token = await createAccessToken({id: userSaved._id});
+         res.cookie('token', token)
+         res.json('Usuario creado satisfactoriamente.');
 
-         await newUser.save();
-         res.send('registrando...')
-         console.log(newUser);
+         
      } catch (error) {
-        console.log(error);
-     }
-
-     
+        console.error(error);
+        res.status(500).json({message0:'Error al registrar usuario', message: error.message});
+     }     
 };
 
-export const login = (req,res) => res.send('login');
+export const login = async (req,res) => {
+   const {email , password} = req.body;
+   try {
+      const userFound = await User.findOne({ email })
+      if(!userFound) return res.status(500).json({ message: 'User not found' })
+      
+      const isMatch = await bcrypt.compare(password , userFound.password);
+      if(!isMatch) return res.status(400).json({ message: 'Incorrect password' });
+
+      const token = await createAccessToken({ id: userFound._id});
+
+      res.cookie("token", token);
+      res.json({
+         id: userFound._id,
+         username: userFound.username,
+         email: userFound.email
+      })
+   }catch(error){
+      res.status(500).json({ message: error.message });
+   }
+  
+
+};
+
+export const logout = (req, res) => {
+   res.cookie('token', '',{
+      expires: new Date(0)
+   });
+   return res.sendStatus(200);
+}
